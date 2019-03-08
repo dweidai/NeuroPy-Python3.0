3662e7e9af8832a0ec58cdbec48a2e86ace70323
David Dai <d4dai@ucsd.edu>
2019-03-08 14:11:06 -0800

    add test file

---
+++ testing.py
@@ -0,0 +1,41 @@
+from NeuroPy.NeuroPy import NeuroPy
+import keyboard
+import serial
+import time
+
+'''
+    My machine is macOS Mojave Version 10.14.3
+    My python is python 3.7
+    My machine is Neurosky Mindwave Mobile 2
+'''
+print("program started")
+f = open("testfile.txt", "w")
+headset = None
+print(headset)
+headset = NeuroPy("/dev/cu.MindWaveMobile-SerialPo", 57600)
+print(headset)
+print("heatset initialized")
+headset.start()
+print("headset started")
+
+while True:
+    att = headset.attention
+    med = headset.meditation
+    f.write(str(att)+"\t" + str(med) +"\n")
+    time.sleep(0.1)
+    print("Attention: ", att)
+    print("Meditation: ", med)
+    time.sleep(0.4)
+
+    try:
+        if keyboard.is_pressed('q'):
+            break
+        else:
+            pass
+    except:
+        print("\t!!!!\nquit program\n\t!!!!!!\n")
+        break  # if user pressed a key other than the given key the loop will break
+
+headset.stop()
+print(headset)
+print("headset closed")
